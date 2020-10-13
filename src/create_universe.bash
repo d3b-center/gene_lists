905ab8cc21578f9ea054fd771a47f84e91fbf481
Jo Lynne Rokita <jolynnerokita@d3b.center>
2020-10-13 15:11:09 -0400

    update universe genelist
    update path, genelist, and readme

--- src/create_universe.bash
+++ src/create_universe.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-wget ftp://ftp.ebi.ac.uk/pub/databases/genenames/locus_types/gene_with_protein_product.txt.gz
-gunzip gene_with_protein_product.txt.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/tsv/locus_types/gene_with_protein_product.txt
 cat gene_with_protein_product.txt | cut -f2 | tail -n +2 | sort | uniq > lists/universe.tsv
